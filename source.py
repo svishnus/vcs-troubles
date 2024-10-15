fa2b645e782c4b7effa855ebae6c4da3eee2d0cf
Sundara Vishnu Satish <svishnus@umich.edu>
2024-10-15 17:31:21 -0500

    relocation conflict

--- source.py
+++ source.py
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 def sum(a: str, b: str) -> str:
     return a + b
 
 
+=======
+>>>>>>> d12ec4e (alice move func1 below func2)
 def func_2(a: str, b: str) -> str:
     return b + a
+
+
+def func_1(a: str, b: str) -> str:
+    return a + b
