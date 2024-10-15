23db08175b7c254e97c858e37480a3399520750b
Sundara Vishnu Satish <svishnus@umich.edu>
2024-10-15 17:21:02 -0500

    granularity conflict

--- source.py
+++ source.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 def func_1(a: str, b: str, c="") -> str:
     return a + b + c
+=======
+def add(a: str, b: str) -> str:
+    return a + b
+>>>>>>> 87bc35a (alice rename func1 to add)
 
 
 def func_2(a: str, b: str) -> str:
