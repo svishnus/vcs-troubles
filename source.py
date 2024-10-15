5067ef4627340157e552f0335aaccca1b5f6ca16
Sundara Vishnu Satish <svishnus@umich.edu>
2024-10-15 17:14:59 -0500

    fix: remove other funcs

--- source.py
+++ source.py
@@ -4,12 +4,3 @@ def func_1(a: str, b: str) -> str:
 
 def func_2(a: str, b: str) -> str:
     return b + a
-
-
-def test():
-    print(func_1("a", "b"))
-    print(func_2("a", "b"))
-
-
-if __name__ == "__main__":
-    test()
